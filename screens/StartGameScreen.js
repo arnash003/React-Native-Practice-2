8497b47a87d7af1d3b3dad73249c270dbee7d60c
Asher-jay Arenz <asher-jayarenz@MacBook-Pro-2.local>
2023-08-20 22:36:42 +0900

    styling

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,15 +1,21 @@
-import { View, Text, TextInput, StyleSheet } from 'react-native'
-import React from 'react'
-import PrimaryButton from '../components/PrimaryButton'
+import { View, Text, TextInput, StyleSheet } from "react-native";
+import React from "react";
+import PrimaryButton from "../components/PrimaryButton";
 
 export default function StartGameScreen() {
     return (
         <View style={styles.inputContainer}>
-            <TextInput />
+            <TextInput
+                style={styles.numberInput}
+                maxLength={2}
+                keyboardType="number-pad"
+                autoCapitalize="none"
+                autoCorrect={false}
+            />
             <PrimaryButton>Reset</PrimaryButton>
             <PrimaryButton>Confirm</PrimaryButton>
         </View>
-    )
+    );
 }
 
 const styles = StyleSheet.create({
@@ -23,6 +29,17 @@ const styles = StyleSheet.create({
         shadowColor: "black",
         shadowOffset: { width: 0, height: 2 },
         shadowRadius: 6,
-        shadowOpacity: 0.50,
-    }
+        shadowOpacity: 0.5,
+    },
+    numberInput: {
+        height: 50,
+        width: 50,
+        textAlign: "center",
+        fontSize: 32,
+        borderBottomColor: "#ddb52f",
+        borderBottomWidth: 2,
+        color: "#ddb52f",
+        marginVertical: 8,
+        fontWeight: "bold",
+    },
 });
